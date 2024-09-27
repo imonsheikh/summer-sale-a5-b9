c876dd1f91233fc027d9bb6dd47174f26ba251a7
imon-sheikh <mdimonhossain2017@gmail.com>
2024-09-27 20:24:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// function concatenateArrayToString(arr) {
+//   let result = "";
+//   for (const element of arr) {
+//     result += element;
+//   }
+//   return result;
+// }
+
+// const array = ["Hello", " ", "World", "!"];
+// const concatenatedString = concatenateArrayToString(array);
+// console.log(concatenatedString);
+
+
+// practice 
+const cards = document.querySelectorAll('.card')
+// console.log(cards);
+
+for(let index = 0; index < cards.length; index++){
+  const card = cards[index]
+  // console.log(card);
+  card.addEventListener('click', function(){
+    // console.log('clicked');
+
+    //get the title
+    const title = card.querySelector('h3').innerText
+    // console.log(title);
+    // console.log(title.innerText);
+    const price = card.querySelector('span').innerHTML
+    // console.log(price);
+    // console.log(price.innerText);
+    
+    const titleContainer = document.getElementById('title-container')
+    console.log(titleContainer);
+    
+    const p = document.createElement('p')
+    p.innerText = title
+
+    titleContainer.appendChild(p)
+    console.log(titleContainer);
+    
+  })
+  
+}
