e80dee6b57b47bdc007c078ee9d72c96483d0ce3
imon-sheikh <mdimonhossain2017@gmail.com>
2024-09-28 13:26:28 +0600

    total price and discount part is completed

--- index.js
+++ index.js
@@ -10,45 +10,78 @@
 // const concatenatedString = concatenateArrayToString(array);
 // console.log(concatenatedString);
 
-
-// practice 
+// practice
 let titleCount = 1;
-let totalPrice = 0
+let totalPrice = 0;
 
-const cards = document.querySelectorAll('.card')
+const cards = document.querySelectorAll(".card");
 // console.log(cards);
 
-for(let index = 0; index < cards.length; index++){
-  const card = cards[index]
+for (let index = 0; index < cards.length; index++) {
+  const card = cards[index];
   // console.log(card);
-  card.addEventListener('click', function(){
+  card.addEventListener("click", function () {
     // console.log('clicked');
 
     //get the title
-    const title = card.querySelector('h3').innerText
+    const title = card.querySelector("h3").innerText;
     // console.log(title);
     // console.log(title.innerText);
-    const price = parseFloat(card.querySelector('span').innerHTML.split(" ")[1])
+    const price = parseFloat(
+      card.querySelector("span").innerHTML.split(" ")[1]
+    );
     console.log(price);
     // console.log(price.innerText);
-    
-    const titleContainer = document.getElementById('title-container')
-    console.log(titleContainer) ;
-    
-    const p = document.createElement('p')
-    p.innerText = titleCount + ". " + title
 
-    titleContainer.appendChild(p)
+    const titleContainer = document.getElementById("title-container");
     console.log(titleContainer);
-    titleCount++
 
+    const p = document.createElement("p");
+    p.innerText = titleCount + ". " + title;
+
+    titleContainer.appendChild(p);
+    console.log(titleContainer);
+    titleCount++;
 
     //calculate price
-    totalPrice += price
+    totalPrice += price;
     console.log(totalPrice);
-    document.getElementById('totalPrice').innerText = totalPrice.toFixed(2)
-    
-    
-  })
-  
+    document.getElementById("totalPrice").innerText = totalPrice.toFixed(2);
+  });
 }
+
+const btn = document.getElementById("apply-btn");
+btn.addEventListener("click", function () {
+  //get the value from input
+  const couponElement = document.getElementById("input-field").value;
+  console.log(typeof couponElement);
+
+  const couponCode = couponElement.split(" ").join("").toUpperCase();
+  console.log(couponCode); //SELL200
+
+  if (totalPrice >= 200) {
+    // console.log(couponElement);
+    if (couponCode === "SELL200") {
+      //discount calculation
+      const discountElement = document.getElementById("discountPrice");
+      const discountAmount = totalPrice * 0.2;
+      discountElement.innerText = discountAmount.toFixed(2);
+
+      //rest total calculation
+      const restTotal = document.getElementById("total");
+      restTotal.innerText = (totalPrice - discountAmount).toFixed(2);
+
+      //clear the inputField
+      document.getElementById("input-field").value = "";
+    } else {
+      alert("Invalid coupon Code");
+
+      //clear the inputField
+       document.getElementById("input-field").value = "";
+    }
+  } else {
+    alert("Please at least $200 spend brother for get discount");
+     //clear the inputField
+     document.getElementById("input-field").value = "";
+  }
+});
